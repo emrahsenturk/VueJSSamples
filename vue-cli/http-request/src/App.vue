2ac521c86c43ab1ea3087ebeae03cb731c9d79d1
emsenturk <emsenturk@cits.com.tr>
2020-09-11 17:29:50 +0300

    Created http-request sample.

---
+++ vue-cli/http-request/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="container">
+    <div class="row">
+      <div class="col-md-8 offset-md-2 text-center">
+        <h3 class="mt-5">ToDo List</h3>
+        <hr>
+        <div class="row">
+          <div class="col-md-6 offset-md-3 d-flex flex-row justify-content-between align-items-center">
+            <input type="text">
+            <button class="btn btn-primary">Ekle</button>
+          </div>
+        </div>
+        <hr>
+        <div class="todo-container">
+          <Todo />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Todo from "@/components/Todo"
+export default {
+  components: {
+    Todo
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-cli/http-request/src/components/Todo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="container">
+    <div class="row">
+        <div class="col-md-6 offset-md-3 border d-flex flex-row justify-content-between align-items-center pt-2 pb-2">
+            <span>ToDo Tanımı</span>
+            <button class="btn btn-danger">Sil</button>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
