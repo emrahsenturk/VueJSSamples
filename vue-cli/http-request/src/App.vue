520731e48d7ae889311246cf1fcce8ab87c41da8
emsenturk <emsenturk@cits.com.tr>
2020-09-14 11:25:29 +0300

    Http request sample done.

--- vue-cli/http-request/src/App.vue
+++ vue-cli/http-request/src/App.vue
@@ -6,13 +6,13 @@
         <hr>
         <div class="row">
           <div class="col-md-6 offset-md-3 d-flex flex-row justify-content-between align-items-center">
-            <input type="text">
-            <button class="btn btn-primary">Ekle</button>
+            <input type="text" v-model="toDoText">
+            <button @click="addToDo()" class="btn btn-primary">Ekle</button>
           </div>
         </div>
         <hr>
         <div class="todo-container">
-          <Todo />
+          <Todo v-for="todo in toDoList" :key="todo.id" :todo="todo" @deletetodo="deleteToDo($event)" />
         </div>
       </div>
     </div>
@@ -21,9 +21,60 @@
 
 <script>
 import Todo from "@/components/Todo"
+import axios from "axios"
 export default {
   components: {
     Todo
+  },
+  data(){
+    return {
+      toDoText: "",
+      toDoList: []
+    };
+  },
+  methods: {
+    addToDo(){
+      axios.post("https://vuejs-http-request-7bb50.firebaseio.com/todos.json", { text: this.toDoText })
+        .then(response => {
+          console.log(response);
+          this.toDoList.push({
+            id: response.data.name,
+            text: this.toDoText
+          });
+        })
+        .catch(e => {
+          console.log(e);
+        })
+    },
+    deleteToDo(todoId){
+      axios.delete("https://vuejs-http-request-7bb50.firebaseio.com/todos/" + todoId + ".json")
+        .then(response => {
+          let index = this.toDoList.findIndex(i => {
+              return i.id == todoId;
+          });
+          this.toDoList.splice(index, 1);
+        })
+        .catch(e => {
+          console.log(e);
+        })
+    }
+  },
+  created() {
+    axios.get("https://vuejs-http-request-7bb50.firebaseio.com/todos.json")
+      .then(response => {
+        console.log(response.data);
+        for(let key in response.data){
+          console.log(response.data[key]);
+          let todo = {
+            id: key,
+            text: response.data[key].text
+          };
+          this.toDoList.push(todo);
+        }
+      })
+      .catch(e => {
+        console.log(e);
+      })
   }
 }
 </script>
--- vue-cli/http-request/src/components/Todo.vue
+++ vue-cli/http-request/src/components/Todo.vue
@@ -2,8 +2,8 @@
   <div id="container">
     <div class="row">
         <div class="col-md-6 offset-md-3 border d-flex flex-row justify-content-between align-items-center pt-2 pb-2">
-            <span>ToDo Tanımı</span>
-            <button class="btn btn-danger">Sil</button>
+            <span>{{ todo.text }}</span>
+            <button class="btn btn-danger" @click="$emit('deletetodo', todo.id)">Sil</button>
         </div>
     </div>
   </div>
@@ -11,10 +11,7 @@
 
 <script>
 export default {
-
+  props: ["todo"]
 }
 </script>
 
-<style>
-
-</style>
